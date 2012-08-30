dcc9b6a1b88586e6f4bccd3cf26a0f51b3e0402f
“baden” <baden.i.ua@gmail.com>
2012-08-30 15:48:14 +0300

    Правка скрипта инициализации.

--- init-scripts/init0.sh
+++ init-scripts/init0.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
+if [[ $EUID -ne 0 ]]; then
+   echo "Fail. This script must be run as root." 1>&2
+   exit 1
+fi
 
-echo "Script is under construction."
+apt-get update
+apt-get upgrade
+apt-get install mc python-pip
+nginx=stable # use nginx=development for latest development version
+add-apt-repository ppa:nginx/$nginx
+apt-get update
+apt-get install nginx
+
+echo "All done OK."
